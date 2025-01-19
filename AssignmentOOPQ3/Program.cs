d047f426d2eb96c0a79fc0d1378f452a010a645c
YaraBasha <yarahossam4@gmail.com>
2025-01-19 15:05:59 +0200

    solution for Q2

--- AssignmentOOPQ3/Program.cs
+++ AssignmentOOPQ3/Program.cs
@@ -3,15 +3,44 @@
 
 
 
-class Program
+interface IAuthenticationService
 {
- static void Main(string[] args)
+    bool AuthenticateUser(string username, string password);
+    bool AuthorizeUser(string username, string role);
+}
+class BasicAuthenticationService : IAuthenticationService
+{
+    public bool AuthenticateUser(string username, string password)
     {
-        
+        return username == "yara" && password == "12345";
+    }
+    public bool AuthorizeUser(string username, string role)
+    {
+        return username == "yara" && role == "Admin";
     }
 }
 
-//output:Email sent to yara@yahoo.com: Hello via Email!
-//SMS sent to +0123456789: Hello via SMS!
-//Push notification sent to Yara's Device: Hello via Push Notification! 
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAuthenticationService authService = new BasicAuthenticationService();
+
+        if (authService.AuthenticateUser("yara", "12345"))
+        {
+            Console.WriteLine("Authentication Successful!");
+            if (authService.AuthorizeUser("yara", "Admin"))
+                Console.WriteLine("Authorization Successful!");
+            else
+                Console.WriteLine("Authorization Failed!");
+        }
+        else
+        {
+            Console.WriteLine("Authentication Successful!");
+        }
+    }
+}
+//result: Authentication Successful!
+//Authorization Successful!
+
 
