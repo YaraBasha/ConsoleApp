0eaf84ae6b6b75b05774f653650dedb14fdad940
YaraBasha <yarahossam4@gmail.com>
2025-01-19 15:11:42 +0200

    Question 1

--- AssignmentOOPQ3/Program.cs
+++ AssignmentOOPQ3/Program.cs
@@ -3,15 +3,54 @@
 
 
 
-class Program
+interface IShape
 {
- static void Main(string[] args)
+    double Area { get; }
+    void DisplayShape();
+}
+
+class Circle : IShape
+{
+    public double Radius { get; set; }
+    public Circle(double radius)
     {
-        
+        Radius = radius;
+    }
+    public double Area => Math.PI * Radius * Radius;
+    public void DisplayShape()
+    {
+        Console.WriteLine($"Circle: Radius = {Radius}, Area = {Area:F2}");
     }
 }
 
-//output:Email sent to yara@yahoo.com: Hello via Email!
-//SMS sent to +0123456789: Hello via SMS!
-//Push notification sent to Yara's Device: Hello via Push Notification! 
+class Rectangle : IShape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+    public double Area => Length * Width;
+    public void DisplayShape()
+    {
+        Console.WriteLine($"Rectangle: Length = {Length}, Width = {Width}, Area = {Area:F2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IShape circle = new Circle(5);
+        circle.DisplayShape();
+
+        IShape rectangle = new Rectangle(10, 4);
+        rectangle.DisplayShape();
+    }
+}
+// Result : Circle: Radius = 5, Area = 78.54
+//Rectangle: Length = 10, Width = 4, Area = 40.00
 
