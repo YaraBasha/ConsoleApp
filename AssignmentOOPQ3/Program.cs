4cb500b49ce38c1e5cb5209d0077a54e5c75edad
YaraBasha <yarahossam4@gmail.com>
2025-01-19 15:00:57 +0200

    removed what is in the class program

---
+++ AssignmentOOPQ3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+
+class Program
+{
+ static void Main(string[] args)
+    {
+        
+    }
+}
+
+//output:Email sent to yara@yahoo.com: Hello via Email!
+//SMS sent to +0123456789: Hello via SMS!
+//Push notification sent to Yara's Device: Hello via Push Notification! 
+
